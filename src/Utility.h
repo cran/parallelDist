168401ae83a2360f0c62575ac69941504dbe2b42
Alexander Eckert <info@alexandereckert.com>
2017-06-07 20:35:41 +0000

    version 0.1.1

--- src/Utility.h
+++ src/Utility.h
@@ -22,17 +22,14 @@
 
 #include <memory>
 #include <cmath>
+#include <string>
 
 namespace utility {
-
-template<typename T, typename... Args>
-std::unique_ptr<T> make_unique(Args&&... args) {
-    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
-}
 
 bool isEqualStr(std::string& str1, std::string str2);
 
 double similarityToDistance(const double distance);
+
 }
 
 #endif
